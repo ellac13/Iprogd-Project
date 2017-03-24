fe7587cfd6e06f3fd1fffc8ac1c7c22e192f2317
Isak <isak.stenso@gmail.com>
2017-03-24 14:52:36 +0100

    Dummy login/logout functionality
    4 users: isak, philip, calle, andreas. Pwd is same as username

--- app/js/loginCtrl.js
+++ app/js/loginCtrl.js
@@ -10,6 +10,15 @@ phisancaApp.controller('LoginCtrl', function ($scope) {
   $scope.noUsername = false;        //An empty username was sent
   $scope.noPwd = false;             //An empty pwd was sent
 
+  $scope.loggedIn = false;
+  $scope.currentUser = "";
+  $scope.users = {
+    isak:"isak",
+    calle:"calle",
+    philip:"philip",
+    andreas:"andreas"
+  };
+
   $scope.login = function (u, pwd) {
 		if (u === "") {
       $scope.usernamePlaceholder = "Enter a username";
@@ -18,20 +27,37 @@ phisancaApp.controller('LoginCtrl', function ($scope) {
       $scope.pwdPlaceholder = "Enter a password";
       $scope.noPwd = true;
     } else {
-      //Log in
+      if ($scope.users[u] == pwd) {
+        $scope.loggedIn = true;
+        $scope.currentUser = u;
+      } else {
+        $scope.noUsername = true;
+        $scope.username = "";
+        $scope.pwd = "";
+        $scope.usernamePlaceholder = "Incorrect details";
+      }
     }
 	};
 
   $scope.usernameChanged = function() {
     $scope.noUsername = false;
+    $scope.usernamePlaceholder = "username";
   }
 
   $scope.pwdChanged = function() {
     $scope.noPwd = false;
+    $scope.pwdPlaceholder = "password";
   }
 
   $scope.registerButton = function() {
     alert("Register...");
+  }
+
+  $scope.logout = function() {
+    $scope.loggedIn = false;
+    $scope.currentUser = "";
+    $scope.username = "";
+    $scope.pwd = "";
   }
 
 });
