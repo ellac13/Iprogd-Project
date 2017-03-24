9029b4d97a2607ffc15e7596c11cf95e631bfd5e
ellac13 <calle.13@hotmail.com>
2017-03-24 11:36:58 +0100

    ng-repeat and controller update

--- app/js/locationCtrl.js
+++ app/js/locationCtrl.js
@@ -1,11 +1,21 @@
 phisancaApp.controller('LocationCtrl', function ($scope,Weather) {
 
+	$scope.favouriteLocations = ["Stockholm", "Nyköping"];
+	$scope.popularLocations = ["Göteborg", "Malmö"];
+	$scope.recentLocations = ["Boden", "Jönköping"];
+
+
 	$scope.testGeo = function() {
 		Weather.testGeolocation();
 	}
 
 	$scope.favourite = function($event){
-		//alert("favorited");
+		//alert("favourited");
+		//console.log($event.currentTarget);
+	}
+
+	$scope.unfavourite = function($event){
+		//alert("unfavourited");
 		//console.log($event.currentTarget);
 	}
 
