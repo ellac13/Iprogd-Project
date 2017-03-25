a998223850573909945a36f34b1053c7252f1a7e
Andreas Rosenback <rosenba@kth.se>
2017-03-25 08:59:58 +0100

    Using scope instead of this

--- app/js/tabsCtrl.js
+++ app/js/tabsCtrl.js
@@ -1,12 +1,12 @@
 
 phisancaApp.controller('TabsCtrl', function ($scope) {
-	this.tab = 1;
+	$scope.tab = 1;
 
-	this.setTab = function (tabId) {
-		this.tab = tabId;
+	$scope.setTab = function (tabId) {
+		$scope.tab = tabId;
 	};
 
-	this.isSet = function (tabId) {
-		return this.tab === tabId;
+	$scope.isSet = function (tabId) {
+		return $scope.tab === tabId;
 	};
 });
