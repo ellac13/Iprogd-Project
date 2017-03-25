033530a94ff0c221053aff5c10c44f3678a94768
Andreas Rosenback <rosenba@kth.se>
2017-03-25 09:51:54 +0100

    Tabs using ng-repeat instead

--- app/js/tabsCtrl.js
+++ app/js/tabsCtrl.js
@@ -1,12 +1,19 @@
 
 phisancaApp.controller('TabsCtrl', function ($scope) {
-	$scope.tab = 1;
+	
+	$scope.tabs = [{title: 'maps'}, {title: 'daily'}, {title: 'hourly'}];
+	
+	$scope.tab = 0;
 
 	$scope.setTab = function (tabId) {
 		$scope.tab = tabId;
 	};
 
 	$scope.isSet = function (tabId) {
-		return $scope.tab === tabId;
+		for(var i = 0; i < $scope.tabs.length; i++){
+			if(tabId === $scope.tab){	
+				return $scope.tabs[tabId];
+			}
+		}
 	};
 });
