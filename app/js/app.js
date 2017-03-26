ef1544206352b08b943120dfc35df508ab8e0907
Isak <isak.stenso@gmail.com>
2017-03-26 15:37:08 +0200

    Added register button, with popup

--- app/js/app.js
+++ app/js/app.js
@@ -10,7 +10,7 @@
 // into developing Angular applications. For this lab, these two will suffice.
 
 var phisancaApp = angular.module('phisanca',
-  ['ngRoute','ngResource','ngCookies','uiGmapgoogle-maps','rzModule','firebase']);
+  ['ngRoute','ngResource','ngCookies','uiGmapgoogle-maps','rzModule','firebase','ngMaterial']);
 
 
 phisancaApp.config(function(uiGmapGoogleMapApiProvider) {
--- app/js/loginCtrl.js
+++ app/js/loginCtrl.js
@@ -1,5 +1,5 @@
 
-phisancaApp.controller('LoginCtrl', function ($scope,Weather) {
+phisancaApp.controller('LoginCtrl', function ($scope,Weather,$mdDialog) {
 
   $scope.usernamePlaceholder = "username";
   $scope.pwdPlaceholder = "password";
@@ -37,21 +37,90 @@ phisancaApp.controller('LoginCtrl', function ($scope,Weather) {
   $scope.usernameChanged = function() {
     $scope.noUsername = false;
     $scope.usernamePlaceholder = "username";
-  }
+  };
 
   $scope.pwdChanged = function() {
     $scope.noPwd = false;
     $scope.pwdPlaceholder = "password";
-  }
+  };
 
-  $scope.registerButton = function() {
-    alert("Register...");
-  }
+  $scope.registerButton = function(ev) {
+    // alert("Register...");
+    $mdDialog.show({
+      controller: DialogController,
+      templateUrl: 'partials/registerDialog.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose:true,
+      fullscreen: $scope.customFullscreen // Only for -xs, -sm breakpoints.
+    })
+    .then(function() {
+    });
+  };
 
   $scope.logout = function() {
     $scope.auth.$signOut();
     $scope.username = "";
     $scope.pwd = "";
+  };
+
+  function DialogController($scope, $mdDialog) {
+    $scope.emailPlaceholder = "username";
+    $scope.passPlaceholder = "password";
+    $scope.email = "";
+    $scope.pass = "";
+    $scope.noEmail = false;        //An empty username was sent
+    $scope.noPass = false;             //An empty pwd was sent
+    $scope.status = "lol";
+    $scope.error = false;
+    $scope.auth = Weather.getAuth();
+
+    $scope.hide = function() {
+      $mdDialog.hide();
+    };
+
+    $scope.cancel = function() {
+      $mdDialog.cancel();
+    };
+
+    $scope.answer = function(answer) {
+      $mdDialog.hide(answer);
+    };
+
+    $scope.register = function(email, pwd) {
+      $scope.error = false;
+      if (email === "") {
+        $scope.emailPlaceholder = "Enter a username";
+        $scope.noEmail = true;
+      } else if (pwd === "") {
+        $scope.passPlaceholder = "Enter a password";
+        $scope.noPass = true;
+      } else {
+        $scope.auth.$createUserWithEmailAndPassword(email, pwd)
+          .then(function(firebaseUser) {
+            alert("User " + firebaseUser.uid + " created successfully!");
+            console.log("User " + firebaseUser.uid + " created successfully!");
+            $scope.answer();
+          }).catch(function(error) {
+            $scope.email = "";
+            $scope.pass = "";
+            $scope.error = true;
+            $scope.status = error;
+            alert(error);
+            console.error("Error: ", error);
+          });
+      }
+    };
+
+    $scope.emailChanged = function() {
+      $scope.noEmail = false;
+      $scope.emailPlaceholder = "username";
+    };
+
+    $scope.passChanged = function() {
+      $scope.noPass = false;
+      $scope.passPlaceholder = "password";
+    };
   }
 
 });
